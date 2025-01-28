c534c59f8e7c52ffc9dbefd33e3ef9cfd648e8ce
muhammad-osama-rasheed <muhammadosama0080@gmail.com>
2025-01-28 22:46:07 +0500

    second version

--- routes/progressRoutes.js
+++ routes/progressRoutes.js
@@ -8,7 +8,6 @@ router.post("/", async (req, res) => {
     const newProgress = new Progress(data);
 
     const savedProgress = await newProgress.save();
-    console.log("DATA SAVED!");
     res.status(201).json({ success: true, data: savedProgress });
   } catch (error) {
     console.error("Error Saving: ", error);
@@ -21,12 +20,50 @@ router.get("/", async (req, res) => {
     const data = await Progress.find();
 
     if (data.length == 0) {
-      return res.status(404).json("Data Not Found.");
+      return res.status(404).json({ error: "Data Not Found." });
     }
 
     res.status(201).json(data);
   } catch (error) {
     console.error("Error Fetching: ", error);
+    res.status(500).json({ error: "Internal Server Error." });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const updateData = req.body;
+    const updatedProgress = await Progress.findByIdAndUpdate(id, updateData, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!data) {
+      return res.status(404).json({ error: "Data Not Found." });
+    }
+
+    res.status(200).json({ success: true, data: updatedProgress });
+  } catch (error) {
+    console.error("Error Updating: ", error);
+    res.status(500).json({ error: "Internal Server Error." });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const response = await Progress.findByIdAndDelete(id);
+
+    if (!response) {
+      return res.status(404).json({ error: "Data Not Found." });
+    }
+
+    res
+      .status(200)
+      .json({ success: true, message: "Data Deleted Successfully." });
+  } catch (error) {
+    console.error("Error Deleting: ", error);
     res.status(500).json({ error: "Internal Server Error." });
   }
 });
