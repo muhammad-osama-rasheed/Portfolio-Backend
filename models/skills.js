789a77631b95155b9321e7b0deb2a08efba1d951
muhammad-osama-rasheed <muhammadosama0080@gmail.com>
2025-01-31 20:38:45 +0500

    6th version

--- models/skills.js
+++ models/skills.js
@@ -1,33 +1,28 @@
 const mongoose = require("mongoose");
 
-const skillsSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-      min: 1,
-      max: 25,
-      default: "Skill",
-    },
-
-    category: {
-      type: String,
-      required: true,
-      enum: ["frontend", "backend", "database", "language", "other"],
-      trim: true,
-    },
-
-    image: {
-      type: String,
-      default:
-        "https://drive.google.com/file/d/1vcrzoqojCCnDJfrbHu6g3qKLgd98hIho/view?usp=sharing",
-    },
+const skillsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+    min: 1,
+    max: 25,
+    default: "Skill",
   },
-  {
-    timestamps: true,
-  }
-);
+
+  category: {
+    type: String,
+    required: true,
+    enum: ["frontend", "backend", "database", "language", "other"],
+    trim: true,
+  },
+
+  image: {
+    type: String,
+    default:
+      "https://drive.google.com/file/d/1vcrzoqojCCnDJfrbHu6g3qKLgd98hIho/view?usp=sharing",
+  },
+});
 
 const Skills = mongoose.model("Skills", skillsSchema);
 module.exports = Skills;
