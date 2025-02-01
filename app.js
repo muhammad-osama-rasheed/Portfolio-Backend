ea58bd9f96ff08f2bdce598b8d0013405ab899c1
muhammad-osama-rasheed <muhammadosama0080@gmail.com>
2025-02-01 12:17:06 +0500

    7th version

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const corsConfig = {
   origin: "*",
   credentials: true,
-  methods: ["GET, POST, PUT, DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 app.options("", cors(corsConfig));
 app.use(cors(corsConfig));
