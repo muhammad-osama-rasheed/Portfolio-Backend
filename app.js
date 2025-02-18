a1674613159a36d51e7aa5a6a32aa3208e9c6b32
muhammad-osama-rasheed <muhammadosama0080@gmail.com>
2025-02-19 00:49:54 +0500

    24th version

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.use("/about", aboutRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "home.html"));
+  res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
 app.listen(port, () => {
